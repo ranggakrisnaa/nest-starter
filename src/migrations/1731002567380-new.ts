86682dd6c9098fbf0f6517c7bf3b2c48f9f636b4
ranggakrisnaa <infinity.duos@gmail.com>
2024-11-08 01:03:08 +0700

    update migration

--- src/migrations/1731002567380-new.ts
+++ src/migrations/1731002567380-new.ts
@@ -1,9 +1,19 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class MyNewMigration1731001649846 implements MigrationInterface {
-    name = 'MyNewMigration1731001649846'
+export class New1731002567380 implements MigrationInterface {
+    name = 'New1731002567380'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            CREATE TABLE "sampletable2" (
+                "id" SERIAL NOT NULL,
+                "title" character varying(255) NOT NULL,
+                "content" text,
+                "updated_at" TIMESTAMP NOT NULL DEFAULT now(),
+                "created_at" TIMESTAMP NOT NULL DEFAULT now(),
+                CONSTRAINT "PK_92c54d7a473051b2527b1656084" PRIMARY KEY ("id")
+            )
+        `);
         await queryRunner.query(`
             CREATE TABLE "sampletable1" (
                 "id" SERIAL NOT NULL,
@@ -15,24 +25,14 @@ export class MyNewMigration1731001649846 implements MigrationInterface {
                 CONSTRAINT "PK_4685a69ad019990e4071c5d5af7" PRIMARY KEY ("id")
             )
         `);
-        await queryRunner.query(`
-            CREATE TABLE "sampletable2" (
-                "id" SERIAL NOT NULL,
-                "title" character varying(255) NOT NULL,
-                "content" text,
-                "updated_at" TIMESTAMP NOT NULL DEFAULT now(),
-                "created_at" TIMESTAMP NOT NULL DEFAULT now(),
-                CONSTRAINT "PK_92c54d7a473051b2527b1656084" PRIMARY KEY ("id")
-            )
-        `);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`
-            DROP TABLE "sampletable2"
+            DROP TABLE "sampletable1"
         `);
         await queryRunner.query(`
-            DROP TABLE "sampletable1"
+            DROP TABLE "sampletable2"
         `);
     }
 
