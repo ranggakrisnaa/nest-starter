64e33fbfd20244b1963cf6e43f470063afaa600b
ranggakrisnaa <infinity.duos@gmail.com>
2024-12-12 06:16:51 +0700

    fix: remove unused module and file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,37 +1,45 @@
 import eslint from '@eslint/js';
-import prettierRecommended from 'eslint-plugin-prettier/recommended';
-import tsEslint from 'typescript-eslint';
+import prettierRecommended from 'eslint-plugin-prettier';
+import tsEslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
 
-export default tsEslint.config(
-  eslint.configs.recommended,
-  ...tsEslint.configs.recommended,
-  prettierRecommended,
-  {
-    languageOptions: {
-      parserOptions: {
-        project: './tsconfig.json',
-        tsconfigRootDir: import.meta.dirname,
-        sourceType: 'module',
-      },
-    },
-    ignores: [
-      'eslint.config.mjs',
-      'docs/.vuepress/**/*',
-      'src/generated/i18n.generated.ts',
-    ],
-    rules: {
-      '@typescript-eslint/interface-name-prefix': 'off',
-      '@typescript-eslint/explicit-function-return-type': 'off',
-      '@typescript-eslint/explicit-module-boundary-types': 'off',
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-unused-vars': [
-        'warn',
-        {
-          argsIgnorePattern: '^_',
-          varsIgnorePattern: '^_',
-          caughtErrorsIgnorePattern: '^_',
-        },
-      ],
-    },
+export default {
+  root: true,
+  parser: tsParser,
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: import.meta.dirname,
+    sourceType: 'module',
   },
-);
+  ignorePatterns: [
+    'eslint.config.mjs',
+    'docs/.vuepress/**/*',
+    'src/generated/i18n.generated.ts',
+  ],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+      },
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
+};
